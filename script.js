1a0334fcaa0d8f512e1149fa154a33a24e2d9916
dev-dede <tsewayog@gmail.com>
2025-12-03 11:24:33 +0000

    Add submit function to calculate button

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
-
-
-
-
 const billAMount = document.getElementById("billAmount");
 const customTipAmount = document.getElementById("customerTipAmount");
 const splitCount = document.getElementById("splitCount");
 
 const totalAmount = document.getElementById("totalAmount");
 const tipAmount = document.getElementById("tipAmount");
-const amountperHead = document.getElementById("amountPerPerson")
+const amountperHead = document.getElementById("amountPerPerson");
 
-function reset(){
-    billAMount.value = "";
-    customTipAmount.value = "";
-    splitCount.value = "";
-
+function reset() {
+  billAMount.value = "";
+  customTipAmount.value = "";
+  splitCount.value = "";
 }
 
-function submit(){
-    const bill = parseFloat(billAMount.value);
-    const tip = parseFloat(customTipAmount.value);
-    const split = splitCount.value;
+function submit() {
+  const bill = parseFloat(billAMount.value);
+  const tip = parseFloat(customTipAmount.value);
+  const split = splitCount.value;
 
-    if (!bill || !tip || !split){
-        alert("some field are empty");
-    }else {
-        const totalBill = parseFloat(bill + tip);
-        const amountperperson = parseFloat(totalBill / split);
+  if (!bill || !tip || !split) {
+    alert("some field are empty");
+  } else {
+    const totalBill = parseFloat(bill + tip);
+    const amountperperson = parseFloat(totalBill / split);
 
-        totalAmount.innerText = `The total amoount is : ${totalBill}`;
-        tipAmount.innerText = `Your tip amount is: ${tip}`;
-        amountperHead.innerHTML = `Each person pays: ${amountperperson}`;
-    }
+    totalAmount.innerText = `The total amoount is : ${totalBill}`;
+    tipAmount.innerText = `Your tip amount is: ${tip}`;
+    amountperHead.innerHTML = `Each person pays: ${amountperperson}`;
+  }
 
-    reset();
+  reset();
 }
 
 function toggleDivisibility() {
@@ -42,22 +37,23 @@ function toggleDivisibility() {
 
   if (serviceRatingRadioButton.checked) {
     serviceRatingInputDiv.style.display = "block";
-  } else {serviceRatingInputDiv.style.display = "none";}
+  } else {
+    serviceRatingInputDiv.style.display = "none";
+  }
 }
 
-    const customTip = document.getElementById("customTip");
-    const serviceRating = document.getElementById("serviceRating");
+const customTip = document.getElementById("customTip");
+const serviceRating = document.getElementById("serviceRating");
 
-    const customInput = document.getElementById("customerTipInput");
-    const ratingInput = document.getElementById("serviceRatingInput");
+const customInput = document.getElementById("customerTipInput");
+const ratingInput = document.getElementById("serviceRatingInput");
 
-    customTip.addEventListener("change", function (){
-        customInput.style.display = "block";
-        ratingInput.style.display = "none";
-    });
+customTip.addEventListener("change", function () {
+  customInput.style.display = "block";
+  ratingInput.style.display = "none";
+});
 
-    serviceRating.addEventListener("change", function () {
-        customInput.style.display = "none";
-        ratingInput.style.display = "block";
-    });
-    
+serviceRating.addEventListener("change", function () {
+  customInput.style.display = "none";
+  ratingInput.style.display = "block";
+});
